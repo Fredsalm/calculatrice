d8352f62dd9865d2ccd7ce8879bcd6d186f1339d
fredsalm <salmsimplon@gmail.com>
2017-12-22 12:51:57 +0100

    premiére fonction js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(main);
 
 function main(){
-var init = 0;
+	var a="";
+	var b="";
 
 	function add(a,b){
 		(a+b);
@@ -20,17 +21,69 @@ var init = 0;
 	};
 
 	function correc(){
-
 		$(".btnCorrec").click(function(){
-			$(".display").html(init);
+			$(".num").html('');
 			
 		});
 	};
-	function input(){
-		$(this.init).html(".display");
+	function btn1(){
+		$('.btn1').click(function(){
+			$('.num').append('1');
+		})
+	};
+	function btn2(){
+		$('.btn2').click(function(){
+			$('.num').append('2');
+		})
+	};
+	function btn3(){
+		$('.btn3').click(function(){
+			$('.num').append('3');
+		})
+	};
+	function btn4(){
+		$('.btn4').click(function(){
+			$('.num').append('4');
+		})
+	};
+	function btn5(){
+		$('.btn5').click(function(){
+			$('.num').append('5');
+		})
+	};
+	function btn6(){
+		$('.btn6').click(function(){
+			$('.num').append('6');
+		})
+	};
+	function btn7(){
+		$('.btn7').click(function(){
+			$('.num').append('7');
+		})
+	};
+	function btn8(){
+		$('.btn8').click(function(){
+			$('.num').append('8');
+		})
+	};
+	function btn9(){
+		$('.btn9').click(function(){
+			$('.num').append('9');
+		})
+	};
+	function btn0(){
+		$('.btn0').click(function(){
+			$('.num').append('0');
+		})
 	};
 
-input();
-correc();
+function allbtn(){
+	correc();btn1();btn2();btn9();btn8();btn7();btn6();btn5();btn4();btn3();btn0();
+};
+//if (+) {
+	//a= $('.num').html();
+		//$('.num').html('');
+//}
+allbtn();
 
 };
